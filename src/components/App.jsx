597f07ed7c35311af5356bac0089fd5e836eaa7d
denisolid <dkolisnichienko321@gmail.com>
2024-07-24 20:03:17 +0000

    active link styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./Header/Header";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import NotFound from "../pages/NotFound/NotFound";
 
 export const App = () => {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
